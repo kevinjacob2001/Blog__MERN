70dfa09646252dd914224aa9af8c2ad370777ce7
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 13:35:59 +0530

    Implemented the navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import './App.css';
+
 import HomePage from './pages/homepage'
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlesPage from './pages/Articlespage';
-import './App.css';
+import NavBar from './Components/NavBar'; 
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <NavBar/>
           <div id="page-body">
             <Route exact path="/" component={HomePage} />
             <Route path="/about" component={AboutPage} />
