93eff16e6b93a8512ff9a270808af1353242f644
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 11:45:09 +0530

    added homepage.jsx

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Hello, welcome to my travel blog</h1>
+      <p>
+      Travel blogging is a popular niche. If you are looking to create your own WordPress travel website, this can play to your favor, as there are so many travel blogs to gather inspiration from.
+In this article, we will look at a number of travel blogs (in no particular order) created with MERN stack. Hopefully, these sites will help inspire the design, layout, navigation, pages, images, sidebars, widgets, and many other aesthetics and features, of your own travel blog. So let’s get started…
+
+
+      </p>
+
+      <br />
+
+      <p>
+          <h3>South Pacific islands</h3>
+        Shaped like a giant sombrero, this lush volcanic island stars in
+        countless South Pacific fantasies. The focal point and best asset of
+        this tropical beauty is its ravishing lagoon in technicolor turquoise.
+        Fish, turtles, sharks, and rays swim in the clear waters, and tiny
+        islets or motus dot the lagoon. The island, of course, is distinctly
+        French, with mouthwatering cuisine to match. Diving and snorkeling are
+        excellent in the surrounding reefs, and hiking trails weave through the
+        palm-studded forests.
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
