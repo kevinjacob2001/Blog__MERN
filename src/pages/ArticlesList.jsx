73bcb40555c95254cee9c0eff12e6c1eecef4481
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 14:49:25 +0530

    Completed displaying the part of essay and added links to each article

--- src/pages/ArticlesList.jsx
+++ src/pages/ArticlesList.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+
+import articleContent from "./article-content";
+
 
 const ArticlesList = () => {
   return (
     <>
-      <h1>Articles List</h1>    
+      <h1>Articles</h1>  
+      {articleContent.map((article,key)=>{
+          return(
+    <Link className="article-list-item"  key={key} to={`/article/${article.name}`}>
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0,150)}...</p>
+    </Link>
+          )
+      })}  
     </>
   );
 };
