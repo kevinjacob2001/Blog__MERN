1a480822cded22a52820e583969055cbf96c8195
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 12:10:31 +0530

    Changed the Functional to Class component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import HomePage from './pages/homepage'
 
-function App() {
-  return (
-    <div className="App">
-  <HomePage/>
-    </div>
-  );
+class App extends Component () {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
