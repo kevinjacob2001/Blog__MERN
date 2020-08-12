beeb2528805dc74c240ee2fc18d2d5795f6a9a2a
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 14:19:28 +0530

    Made a POST req.

--- src/Components/AddCommentForm.jsx
+++ src/Components/AddCommentForm.jsx
@@ -1,23 +1,32 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const AddCommentForm = () => {
+  const [username, setusername] = useState("");
+  const [commentText, setCommentText] = useState("");
 
-    const [username,setusername]=useState("")
-    const [commentText,setCommentText]=useState("")
-
-    const addComment=async()=>{
-const result=await fetch(`/api/articles/${articleName/add-comment`},{method:'post'})
-    }
+  const addComment = async () => {
+    const result = await fetch(`/api/articles/${articleName}/add-comment`, {method: "post"});
+    const body = await result.json();
+  };
 
   return (
     <div id="add-comment-form">
       <h3>Add a Comment.</h3>
       <label>
         Name:
-        <input type="text" value={username} onChange={(e)=>setusername(e.target.value)}/>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        />
       </label>
       <label>
-        <textarea rows="4" cols="50" value={commentText} onChange={(e)=>setCommentText(e.target.value)}/>
+        <textarea
+          rows="4"
+          cols="50"
+          value={commentText}
+          onChange={(e) => setCommentText(e.target.value)}
+        />
       </label>
       <button>Add Comment.</button>
     </div>
