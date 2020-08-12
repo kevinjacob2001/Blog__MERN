e17df21daab9e274c79fe14cf4fca88648c837f4
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 13:13:10 +0530

    Adding method to the POST request to the server

--- src/Components/UpvotesSection.jsx
+++ src/Components/UpvotesSection.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-const UpvotesSection = () => (
-  <div id="upvotes-section">
-    <button>Add Upvote!</button>
-    <p>This post has been upvoted {articleInfo.upvotes} times.</p>
-  </div>
-);
+const UpvotesSection = () => {
+
+const upvoteArticle=async()=>{
+const result=await fetch(`/api/articles/${articleName}/upvote`,{method:"POST"})
+}
+
+  return (
+    <div id="upvotes-section">
+      <button onClick={}>Add Upvote!</button>
+      <p>This post has been upvoted {articleInfo.upvotes} times.</p>
+    </div>
+  );
+};
 
 export default UpvotesSection;
