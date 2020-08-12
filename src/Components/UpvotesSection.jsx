677e8c099e450b84f33ccb78ab7777f2f5342322
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 13:19:58 +0530

    added upvoteArticle() to fetch data

--- src/Components/UpvotesSection.jsx
+++ src/Components/UpvotesSection.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-const UpvotesSection = () => {
+const UpvotesSection = ({articleName,upvotes,setArticleInfo}) => {
 
 const upvoteArticle=async()=>{
-const result=await fetch(`/api/articles/${articleName}/upvote`,{method:"POST"})
+const result=await fetch(`/api/articles/${articleName}/upvote`,{method:"post"})
+const body=await result.json();
+setArticleInfo(body)
 }
+
 
   return (
     <div id="upvotes-section">
       <button onClick={}>Add Upvote!</button>
-      <p>This post has been upvoted {articleInfo.upvotes} times.</p>
+      <p>This post has been upvoted {upvotes} times.</p>
     </div>
   );
 };
