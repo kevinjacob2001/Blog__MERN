b81f6aaec2ae9577fda37ff41bfa26678dd8de64
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 18:51:06 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import HomePage from './pages/homepage'
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import Articlespage from './pages/Articlespage';
-import NavBar from './Components/NavBar'; 
+import NavBar from './Components/NavBar';
 import NotFoundPage from './pages/NotFound';
 
 class App extends Component {
@@ -18,14 +18,14 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <NavBar/>
+          <NavBar />
           <div id="page-body">
             <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesListPage} />
-            <Route path="/article/:name" component={Articlespage} />
-            <Route component={NotFoundPage}/>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={Articlespage} />
+              <Route component={NotFoundPage} />
             </Switch>
           </div>
         </div>
--- src/Components/CommentsList.jsx
+++ src/Components/CommentsList.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CommentsList = ({ comments }) => (
   <>
-  <h3>Comments:</h3>
-    {comments.map((comment,key) => (
+    <h3>Comments:</h3>
+    {comments.map((comment, key) => (
       <div className="comment" key={key}>
         <h4>{comment.username}</h4>
         <p>{comment.text}</p>
