9365bf5a9d0677baac724484e1131f6d57de68b4
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 11:40:35 +0530

    added the empty array as useEffect's second parameter

--- src/pages/Articlespage.jsx
+++ src/pages/Articlespage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import articleContent from "./article-content";
 import ArticleList from "../Components/ArticleList";
 import NotFoundPage from "./NotFound";
@@ -9,6 +9,10 @@ const ArticlePage = ({ match }) => {
 
   const [articleInfo, setArticleInfo] = useState({upvotes:0,comments:[]});
 
+useEffect(()=>{
+  setArticleInfo({upvotes:Math.ceil(Math.random()*10)})
+},[]);
+
   if (!article) return <NotFoundPage />;
 
   const otherArticles = articleContent.filter(
@@ -18,6 +22,7 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
+  <p>This post has been upvoted {articleInfo.upvotes} times.</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
