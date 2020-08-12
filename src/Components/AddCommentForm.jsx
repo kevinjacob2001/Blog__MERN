253e9613cf2dc424e47377fbdb95f587d76ecc70
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 13:50:58 +0530

    Added a basic comment submit form(Internal state yet to be added)

--- src/Components/AddCommentForm.jsx
+++ src/Components/AddCommentForm.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{useState} from "react";
 
-const AddCommentForm=()=>{
+const AddCommentForm = () => {
 
-}
+  return (
+    <div id="add-comment-form">
+      <h3>Add a Comment.</h3>
+      <label>
+        Name:
+        <input type="text" />
+      </label>
+      <label>
+        <textarea rows="4" cols="50" />
+      </label>
+      <button>Add Comment.</button>
+    </div>
+  );
+};
 
 export default AddCommentForm;
