885c31dc0f97fd0de5aefde65639514f914c546c
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 11:20:52 +0530

    added useState({})

--- src/pages/Articlespage.jsx
+++ src/pages/Articlespage.jsx
@@ -1,18 +1,19 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import articleContent from "./article-content";
 import ArticleList from "../Components/ArticleList";
 import NotFoundPage from "./NotFound";
 
 const ArticlePage = ({ match }) => {
-
-
-
   const name = match.params.name;
   const article = articleContent.find((article) => article.name === name);
 
+  const [articleInfo, setArticleInfo] = useState({upvotes:0,comments:[]});
+
   if (!article) return <NotFoundPage />;
 
-  const otherArticles = articleContent.filter((article) => article.name !== name);
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
 
   return (
     <>
@@ -20,7 +21,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
-      
+
       <h2 style={{ textAlign: "center" }}>Other articles.</h2>
       <ArticleList articles={otherArticles} />
     </>
